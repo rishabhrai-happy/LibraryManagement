28b8db05e3d47f5ff311c45f2aa8571ea7dddba8
Hp <rishabhrai980@gmail.com>
2020-09-02 11:26:22 +0530

    use of setOnclickListner

--- app/src/main/java/com/example/librarymanagement/MainActivity.kt
+++ app/src/main/java/com/example/librarymanagement/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.librarymanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login_btn.setOnClickListener{
+          var status=  if(username_et.text.toString().equals("technowordid")
+                && paasword_et.text.toString().equals("password")) "login succesfull" else "login fail"
+            Toast.makeText(this,"status", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
