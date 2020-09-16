ea911f43b2d3276f33a5c3fb727b3697f2e151b2
Hp <rishabhrai980@gmail.com>
2020-09-16 10:23:19 +0530

    changes

--- app/src/main/java/com/example/librarymanagement/MainActivity.kt
+++ app/src/main/java/com/example/librarymanagement/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.librarymanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val login_btn: Button=findViewById(R.id.login_btn)
         login_btn.setOnClickListener{
-          var status=  if(username_et.text.toString().equals("technowordid")
-                && paasword_et.text.toString().equals("password") )"login succesfull" else "login fail"
-            Toast.makeText(this,"status", Toast.LENGTH_SHORT).show()
+
+            val intent=Intent(this,StudentPage::class.java)
+            startActivity(intent)
+
         }
 
     }
